253cf14f4289df0a87cea29728825eeb72ecc7a9
AdamSobieraj <adam.sobieraj@op.pl>
2024-11-03 08:45:55 +0100

    first commit

--- Regresja.py
+++ Regresja.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib.widgets import Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 bike_data_raw = pd.read_csv('data/daily-bike-share.csv')
 bike_data_raw.info()
@@ -37,17 +41,17 @@ categorical_features = ['season','mnth','holiday','weekday','workingday','weathe
 target = 'rentals'
 
 #
-for numeric_feature in numeric_features:
-    fig = plt.figure(figsize=(5, 2))
-    plt.scatter(bike_data[numeric_feature], bike_data[target], alpha=0.25)
-    plt.xlabel(numeric_feature)
-    plt.ylabel('Bike Rentals')
-    plt.title(f'rentals vs {numeric_feature}')
-    plt.show()
+# for numeric_feature in numeric_features:
+#     fig = plt.figure(figsize=(5, 2))
+#     plt.scatter(bike_data[numeric_feature], bike_data[target], alpha=0.25)
+#     plt.xlabel(numeric_feature)
+#     plt.ylabel('Bike Rentals')
+#     plt.title(f'rentals vs {numeric_feature}')
+#     plt.show()
 
 fig = sns.pairplot(bike_data[[target]+numeric_features], kind='scatter', plot_kws={'alpha': 0.25})
 fig.fig.set_size_inches(11, 11) #ustawiamy rozmiar wykresu
-plt.show()
+# plt.show()
 
 plt.figure(figsize=(8, 8))
 ax = sns.heatmap(bike_data[[target]+numeric_features].corr(),
@@ -57,20 +61,269 @@ ax = sns.heatmap(bike_data[[target]+numeric_features].corr(),
                  center=0,
                  annot=True)
 
-plt.title('Korelacja zmiennych numerycznych dla zbioru bike dataset', fontsize=12)
-bottom, top = ax.get_ylim()
-ax.set_ylim(bottom + 0.5, top - 0.5)
-plt.xticks(fontsize=10)
-plt.yticks(fontsize=10)
-plt.show()
+# plt.title('Korelacja zmiennych numerycznych dla zbioru bike dataset', fontsize=12)
+# bottom, top = ax.get_ylim()
+# ax.set_ylim(bottom + 0.5, top - 0.5)
+# plt.xticks(fontsize=10)
+# plt.yticks(fontsize=10)
+# plt.show()
 
 bike_data['difference_temp'] = (bike_data['atemp'] - bike_data['temp'])/bike_data['temp']
 bike_data.drop(['atemp'], axis=1, inplace=True)
 numeric_features = ['temp', 'difference_temp', 'hum', 'windspeed']
 bike_data[['rentals', 'temp', 'difference_temp']].corr()
 
-#Boxplot
-for categoric_features in categorical_features:
-    plt.figure(figsize=(16, 3))
-    sns.violinplot(y=bike_data[target], x=bike_data[categoric_features], palette="Set2")
+# #Boxplot
+# for categoric_features in categorical_features:
+#     plt.figure(figsize=(16, 3))
+#     sns.violinplot(y=bike_data[target], x=bike_data[categoric_features], palette="Set2")
+#     plt.show()
+
+# P2
+#p2
+print(f'Numeric features: {numeric_features}')
+print(f'Categorical features: {categorical_features}')
+print(f'Target: {target}')
+print(bike_data)
+
+X = bike_data[['temp']].copy()
+y = bike_data[target].copy()
+print('X:')
+print(X.values[:3])
+print('\ny:')
+print(y.values[:3])
+
+plt.scatter(X, y, alpha=0.35)
+plt.show()
+
+# Training set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+print(f'X.shape: {X.shape}, y.shape {y.shape}')
+print()
+print(f'X_train.shape: {X_train.shape}, y_train.shape {y_train.shape}')
+print(f'X_test.shape: {X_test.shape}, y_test.shape {y_test.shape}')
+
+#Learning model
+lr_model = LinearRegression() # inicjalizacja modelu
+lr_model.fit(X_train, y_train) # trenowanie modelu
+
+#werification
+print(lr_model.intercept_)
+print(lr_model.coef_)
+
+# Plot
+
+X_linspace = np.linspace(0, 1, 100)
+y_fitted = lr_model.intercept_ + lr_model.coef_ * X_linspace
+
+# Rysowanie wykresu
+plt.scatter(X_train, y_train, color='green', alpha=0.5, label='Zbiór treningowy')
+plt.scatter(X_test, y_test, color='red', alpha=0.5, label='Zbiór testowy')
+plt.plot(X_linspace, y_fitted, color='black', label='Prognoza')
+plt.legend(loc='upper left')
+
+plt.show()
+
+# Prediction
+X_new = [[0.3],
+         [0.7]]
+predicted_data = lr_model.predict(X_new)
+print(predicted_data)
+
+y_predict_train = lr_model.predict(X_train)
+y_predict_test = lr_model.predict(X_test)
+
+print('Predykcja:')
+print(np.round(y_predict_test[:5]).astype(int))
+print('Wartość prawdziwa:')
+print(y_test.values[:5])
+
+from sklearn.metrics import r2_score
+print(f'Train R^2: {r2_score(y_train, y_predict_train)}')
+
+from sklearn.metrics import mean_absolute_error
+print(f'Train MAE: {mean_absolute_error(y_train, y_predict_train)}')
+print(f'Test MAE: {mean_absolute_error(y_test, y_predict_test)}')
+
+#MAPE - Mean Absolute Percentage Error
+from sklearn.metrics import mean_absolute_percentage_error
+print(f'Train MAPE: {mean_absolute_percentage_error(y_train, y_predict_train)}')
+print(f'Test MAPE: {mean_absolute_percentage_error(y_test, y_predict_test)}')
+
+# MSE - Mean Squared Error
+from sklearn.metrics import mean_squared_error
+print(f'Train MSE: {mean_squared_error(y_train, y_predict_train)}')
+print(f'Test MSE: {mean_squared_error(y_test, y_predict_test)}')
+
+# RMSE – Root Mean Squared Error
+from math import sqrt
+print(f'Train RMSE: {sqrt(mean_squared_error(y_train, y_predict_train))}')
+print(f'Test RMSE: {sqrt(mean_squared_error(y_test, y_predict_test))}')
+
+# Plot
+errors = y_predict_test - y_test
+plt.scatter(x = y_test, y = errors, alpha=0.25)
+plt.axhline(0, color="r", linestyle="--")
+plt.xlabel('True Valuey_test')
+plt.ylabel('Residual')
+plt.title(f'Plot of residuals')
+plt.show()
+
+# Histogram reszt
+plt.hist(errors, bins = 20)
+plt.axvline(errors.mean(), color='k', linestyle='dashed', linewidth=1)
+plt.title(f'Histogram of residuals, errors mean = {np.round(errors.mean(), 2)}')
+plt.show()
+
+#Regresja wielomianowa
+from sklearn.preprocessing import PolynomialFeatures
+
+# tworzymy puste listy gdzie umieszczane będą metryki oceniające
+r_2_train_list = []
+rmse_train_list = []
+rmse_test_list = []
+
+# Tworzymy X, który zostanie wykorzystany dla wizualizacji naszego modelu
+X_linespace = np.arange(X.min()[0], X.max()[0], step=0.005).reshape(-1, 1)
+for degree in [1, 2, 3, 5, 10, 20]:
+
+    # Transformacja naszego X
+    poly_transformer = PolynomialFeatures(degree=degree)
+    X_train_transformed = poly_transformer.fit_transform(X_train)
+
+    # Trenowanie naszego modelu
+    polynomial_regression = LinearRegression()
+    polynomial_regression.fit(X_train_transformed, y_train)
+    plt.figure(figsize=(9, 3))
+
+    # Wizualizacja wartości ze zbioru treningowego oraz testowgo
+    plt.scatter(X_train, y_train, color='green', alpha=0.5, label='Zbiór treningowy')
+    plt.scatter(X_test, y_test, color='red', alpha=0.5, label='Zbiór testowy')
+
+    ########## Wizualizacja wielomianu ##########
+    y_fitted = polynomial_regression.predict(poly_transformer.transform(X_linespace))
+    plt.plot(X_linespace, y_fitted, color='black', label='Prognoza')
+    plt.title(f'Degree {degree}')
+    plt.legend(loc='upper left')
     plt.show()
+
+    # Predykcja modelu na zbiorze treningowym
+    prediction_train = polynomial_regression.predict(X_train_transformed)
+
+    # Predykcja modelu na zbiorze testowym - najpierw jednak należy przekształcić zbiór testowy
+    X_test_transformed = poly_transformer.transform(X_test)
+    prediction_test = polynomial_regression.predict(X_test_transformed)
+
+    # Ocena modeli
+    r_2_train_list.append(r2_score(y_train, prediction_train))
+    rmse_train_list.append(sqrt(mean_squared_error(y_train, prediction_train)))
+    rmse_test_list.append(sqrt(mean_squared_error(y_test, prediction_test)))
+
+# Zbiór walidacyjny – sprawdzian krzyżowy
+from sklearn.model_selection import KFold
+cv = KFold(n_splits=5, shuffle=False)
+
+#Pipeline
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+
+# Tworzymy Pipeline - najpierw tworzymy wielomian, następnie uczymy model
+polynomial_regression_pipeline = make_pipeline(PolynomialFeatures(),
+                                               LinearRegression())
+
+# Nasz model sprawdzi te hiperparametry
+params = {'polynomialfeatures__degree': [1, 2, 3, 4, 5]}
+
+# Inicjalizujemy Pipeline
+polynomial_regression_gridsearch = GridSearchCV(polynomial_regression_pipeline,
+                                                params,
+                                                scoring='neg_mean_squared_error', # wybrane będą takie hiperparametry, które dadzą najmniejszą wartość dla tego scoringu
+                                                cv=cv)
+
+# Uczymy Grid Search, podajemy X_train - Pipeline za nas zrobi wielomian :)
+polynomial_regression_gridsearch.fit(X_train, y_train)
+print("\nNajlepsze hiperparametry:", polynomial_regression_gridsearch.best_params_, "\n")
+
+# Przekazujemy najlepszy estymator
+polynomial_regression_model = polynomial_regression_gridsearch.best_estimator_
+predictions = polynomial_regression_model.predict(X_test)
+print(f'RMSE: {np.sqrt(mean_squared_error(y_test, predictions))}')
+
+
+###################################################### dla zadania #################################
+
+#ElasticNet
+# Importujemy regresję liniową z regularyzacją Lasso i Ridge
+from sklearn.linear_model import ElasticNet
+
+# Uczymy model
+polynomial_regression_pipeline = make_pipeline(PolynomialFeatures(),
+                                               ElasticNet())
+
+params = {'polynomialfeatures__degree': [1, 2, 3, 4, 5],
+          'elasticnet__alpha': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.0, 1.0, 10.0, 100.0],
+          'elasticnet__l1_ratio': np.arange(0, 1.1, 0.1)}
+
+polynomial_regression_gridsearch = GridSearchCV(polynomial_regression_pipeline,
+                                                params,
+                                                scoring='neg_mean_squared_error', # wybrane będą takie hiperparametry które dadzą najmniejszą wartość dla tego scoringu
+                                                cv=cv)
+
+polynomial_regression_gridsearch.fit(X_train, y_train)
+print("\nNajlepsze hiperparametry:", polynomial_regression_gridsearch.best_params_, "\n")
+
+polynomial_regression_model = polynomial_regression_gridsearch.best_estimator_
+predictions = polynomial_regression_model.predict(X_test)
+print(f'RMSE: {np.sqrt(mean_squared_error(y_test, predictions))}')
+
+# Ridge Regression
+# Importujemy regresję liniową z regularyzacją Lasso i Ridge
+from sklearn.linear_model import Ridge
+
+# Uczymy model
+polynomial_regression_pipeline = make_pipeline(PolynomialFeatures(),
+                                               Ridge())
+
+params = {'polynomialfeatures__degree': [1, 2, 3, 4, 5],
+          'ridge__alpha': np.logspace(-5, 5, 11)}  # Logarithmic spacing for alpha values
+
+polynomial_regression_gridsearch = GridSearchCV(polynomial_regression_pipeline,
+                                                params,
+                                                scoring='neg_mean_squared_error',
+                                                cv=cv)
+
+polynomial_regression_gridsearch.fit(X_train, y_train)
+print("\nNajlepsze hiperparametry:", polynomial_regression_gridsearch.best_params_, "\n")
+
+polynomial_regression_model = polynomial_regression_gridsearch.best_estimator_
+predictions = polynomial_regression_model.predict(X_test)
+print(f'RMSE: {np.sqrt(mean_squared_error(y_test, predictions))}')
+
+# Lasso Regression
+# Importujemy regresję liniową z regularyzacją Lasso
+from sklearn.linear_model import Lasso
+
+# Uczymy model
+polynomial_regression_pipeline = make_pipeline(PolynomialFeatures(),
+                                               Lasso())
+
+params = {'polynomialfeatures__degree': [1, 2, 3, 4, 5],
+          'lasso__alpha': np.logspace(-5, 5, 11)}  # Logarithmic spacing for alpha values
+
+polynomial_regression_gridsearch = GridSearchCV(polynomial_regression_pipeline,
+                                                params,
+                                                scoring='neg_mean_squared_error',
+                                                cv=cv)
+
+polynomial_regression_gridsearch.fit(X_train, y_train)
+print("\nNajlepsze hiperparametry:", polynomial_regression_gridsearch.best_params_, "\n")
+
+polynomial_regression_model = polynomial_regression_gridsearch.best_estimator_
+predictions = polynomial_regression_model.predict(X_test)
+print(f'RMSE: {np.sqrt(mean_squared_error(y_test, predictions))}')
+
+print("ElasticNet okazał się najlepszym podejściem w tym przypadku, osiągając najniższy RMSE spośród wszystkich modeli.")
+print("Lasso również dało bardzo dobry wynik, jednocześnie zachowując możliwość selekcji cech dzięki swemu mechanizmowi L1 regularization.")
+print("Ridge, mimo że nie osiągnął najlepszego wyniku, nadal zapewnia poprawę wydajności w porównaniu do pierwsi modelu.")
+print("Stopień poprawy wydajności jest różny dla każdego modelu, co może sugerować, że dane mogą być bardziej odpowiednie dla Ridge czy Lasso niż dla ElasticNet.")
+print("Warto rozważyć użycie kombinacji tych technik, np. Elastic Net, aby uzyskać najlepszą wydajność, zachowując przy tym możliwość selekcji cech.")
